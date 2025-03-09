b05a3c2efd9850c78454b2fd87dca685b4cc698e
abbasrizvi2 <abbasali0974@gmail.com>
2025-03-09 23:46:30 +0530

    notes done

--- useRef/src/App.jsx
+++ useRef/src/App.jsx
@@ -1,34 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState,useRef } from 'react'
 import './App.css'
-
+useState
 function App() {
+
   const [count, setCount] = useState(0)
 
+  const btnRef = useRef(0)
+
+  const handleClick = () => {
+    btnRef.current = btnRef.current + 1
+    // setCount((prev) => prev + 1)
+  }
+
+
+  // it runs on every render
+  useEffect(() => {
+    console.log("Screen Render on count" ,count)
+  },)
+
   return (
-    <>
+    
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h3>useRef</h3>
+      <br></br>
+      <h3>Set Count is : {count}</h3>
+      <br></br>
+      <button onClick={handleClick} ref={btnRef}>Increment</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   
   )
 }
 
